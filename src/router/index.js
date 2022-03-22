1ce00864768136c772c5ab2a5776eb3440b08a1e
Bishesh <bishesh.sunam@gmail.com>
2022-03-22 23:40:06 +1300

    auth persistance with firebase

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ChatRoomView from '../views/ChatRoomView.vue'
-import {getAuth} from 'firebase/auth'
+import {getAuth, onAuthStateChanged} from 'firebase/auth'
 
 //auth guard
-const requireAuth =(to, from, next)=> {
-  const auth = getAuth();
-  let currentUser = auth.currentUser
-  if(!currentUser)
+const requireAuth = async(to, from, next)=> {
+  if(! await getCurrentUser())
     next({name: 'home'})
   else
     next()
 }
 
+//get the current User that returns promise
+const getCurrentUser = async()=> {
+  return new Promise((resolve, reject)=> {
+    const removeListener = onAuthStateChanged(
+      getAuth(),
+      (user)=> {
+        removeListener();
+        resolve(user)
+      },
+      reject
+    )
+  })
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
